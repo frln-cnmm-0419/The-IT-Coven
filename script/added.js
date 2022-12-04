9d9f5c16a467b77ef6bee4b2b8af8ee42e86585d
froi <froilanaquino1@gmail.com>
2022-12-05 00:51:37 +0800

    optimization, refactor and redesigning ui elements

--- script/added.js
+++ script/added.js
@@ -3,7 +3,19 @@ $(document).ready(() => {
   var almusalLS = document.getElementsByClassName("almusal");
   var meryendaLS = document.getElementsByClassName("meryenda");
   var pulutanLS = document.getElementsByClassName("pulutan");
-  var sbpr = document.getElementById("snackbar");
+  const sbpr = document.getElementById("snackbar");
+  const paynowBTN = document.getElementsByClassName('pn-btn');
+  const cartCont = document.getElementById('cart-float');
+  const closebtn = document.getElementsByClassName('close-cart');
+
+  $(paynowBTN).click(() => {
+    $(cartCont).css("display", "flex");
+  });
+
+  $(cartCont, closebtn).click(e => {
+    $(cartCont).css("display", "none");
+  })
+
 
   var notif = (product) => {
     $(sbpr).text("SORTED TO " + product + " ONLY");
@@ -16,7 +28,7 @@ $(document).ready(() => {
     $(meryendaLS).css("display", "none");
     $(pulutanLS).css("display", "none");
     notif(targetSort);
-    targetSort == "ALMUSAL" ? $(almusalLS).css("display", "block") : targetSort == "MERYENDA" ? $(meryendaLS).css("display", "block") : $(pulutanLS).css("display", "block");
+    targetSort == "ALMUSAL" ? $(almusalLS).css("display", "flex") : targetSort == "MERYENDA" ? $(meryendaLS).css("display", "flex") : $(pulutanLS).css("display", "flex");
   }
 
   $(sortingbtn).click(e => {
--- script/index.js
+++ script/index.js
@@ -1,35 +1,69 @@
-$(document).ready( e => {
-  const order = document.getElementById('order-now');
+$(document).ready(e => {
   const buttons = document.getElementsByTagName("button");
+  const exit = document.getElementsByClassName('exit');
+  const overlay = document.getElementById('overlay');
+  const others = document.getElementsByClassName('others');
+  const title = document.getElementsByClassName('title');
+  const textcont = document.getElementsByClassName('text');
+
+  const order = document.getElementById("order-now");
+
   for (const button of buttons) {
     button.addEventListener("click", createRipple);
   }
 
-  console.log("hello world");
-  $('#order-now').click(x => {
-    location.href = 'pages/products.html';
-  });
-
   function createRipple(e) {
-  const button = e.currentTarget;
+    const button = e.currentTarget;
 
-  const circle = document.createElement("span");
-  const diameter = Math.max(button.clientWidth, button.clientHeight);
-  const radius = diameter / 2;
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
 
-  circle.style.width = circle.style.height = `${diameter}px`;
-  circle.style.left = `${e.clientX - button.offsetLeft - radius}px`;
-  circle.style.top = `${e.clientY - button.offsetTop - radius}px`;
-  circle.classList.add("ripple");
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${e.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${e.clientY - button.offsetTop - radius}px`;
+    circle.classList.add("ripple");
 
-  const ripple = button.getElementsByClassName("ripple")[0];
+    const ripple = button.getElementsByClassName("ripple")[0];
 
-  if (ripple) {
-    ripple.remove();
+    if (ripple) {
+      ripple.remove();
+    }
+
+    button.appendChild(circle);
   }
 
-  button.appendChild(circle);
-}
+  $(order).click(() => {
+    window.location.href = "pages/products.html";
+  })
 
+  $(exit).click(() => {
+    $(overlay).css('display', 'none');
+  });
 
+  $(overlay).click(() => {
+    $(overlay).css('display', 'none');
+  });
+
+  $(others).click(e => {
+
+    console.log(e.target.id);
+
+    if (e.target.id === "about") {
+      $(title).text("About Us");
+      $(overlay).css("display", "flex");
+      $(textcont).text("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis fugit id asperiores dolorum consequatur, illum quam sapiente voluptatum temporibus impedit blanditiis doloribus molestiae reiciendis rem repellendus voluptates ipsam necessitatibus inventore dicta maxime magnam labore! Quis dolores voluptates placeat eveniet omnis, accusamus unde exercitationem. Aliquid, est reprehenderit! Porro esse optio nostrum.")
+    }
+
+    else if (e.target.id === "faq") {
+      $(title).text("Frequently Asked Questions");
+      $(overlay).css("display", "flex");
+    }
+
+    else if (e.target.id === "stores") {
+      $(title).text("Our Locations");
+      $(overlay).css("display", "flex");
+    }
+
+  });
 });
